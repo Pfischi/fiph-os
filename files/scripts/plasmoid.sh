3e8191cba79b10cb4ae5847b7265e907df49c83f
Philipp Fischer <philippf92@gmail.com>
2025-10-17 22:20:53 +0200

    fix: Use dynamic package name

--- files/scripts/plasmoid.sh
+++ files/scripts/plasmoid.sh
@@ -11,14 +11,14 @@ pushd "$TMPDIR"
 
 echo "Get latest version"
 curl -sSLf "https://api.github.com/repos/luisbocanegra/plasma-panel-colorizer/releases/latest" -o release.json
-NAME=$(jq -r '.assets[0].name' "./release.json")
+NAME_PKG=$(jq -r '.assets[0].name' "./release.json")
 DOWNLOAD_URL=$(jq -r '.assets[0].browser_download_url' "./release.json")
 
-echo "Download latest version"
+echo "Download $DOWNLOAD_URL"
 curl -sSLf -O "$DOWNLOAD_URL"
 
-echo "Install $NAME"
-kpackagetool6 --type=Plasma/Applet --install plasmoid-panel-colorizer-v5.0.1.plasmoid  --global
+echo "Install $NAME_PKG"
+kpackagetool6 --type=Plasma/Applet --install "$NAME_PKG" --global
 
 echo "Delete temp dir"
 pushd
